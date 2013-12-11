2785f21f19dcb13d6bcb43793d908209db0a55c6
Lyndon Maydwell <lyndon@sordina.net>
2013-12-11 15:25:06 +1100

    Better randomization

--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@ import Safe
 import System.Random
 import System.IO
 
-type F = RandomGen g => Int -> Int -> String -> g -> IO ()
+type F = RandomGen g => Int -> Int -> String -> g -> String
 
 main :: IO ()
 main = getArgs >>= args mChars
@@ -26,15 +26,15 @@ start :: F -> Maybe Int -> Maybe Int -> IO ()
 start f (Just con) (Just out) = do
   g <- newStdGen
   i <- getContents
-  f out con i g
+  putStrLn $ f out con i g
 
 start _ _ _ = help
 
 mChars :: F
-mChars out con i g = putStrLn $ take out $ run con i 0 g
+mChars out con i g = drop (con * 10) $ take out $ run con i 0 g
 
 mWords :: F
-mWords out con i g = putStrLn $ unwords $ take out $ run con (words i) 0 g
+mWords out con i g = unwords $ drop (con * 10) $ take out $ run con (words i) 0 g
 
 help :: IO ()
 help = hPutStrLn stderr "Usage: <input> | markov [--help | -h] [--words | -w] <output-length> [context-length]"
